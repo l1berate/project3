9a233ecdce492ba49f04505ac0dd11778219e022
Alec Battest <AlecBattest@gmail.com>
2022-10-05 16:20:45 -0500

    increased quality of images

--- project3-angular/src/app/layout/components/productlist/productlist.component.ts
+++ project3-angular/src/app/layout/components/productlist/productlist.component.ts
@@ -15,7 +15,7 @@ export class ProductlistComponent implements OnInit {
       productCategory:"Computers & Displays",
       productName: "UHD TV TU7000 Series",
       productPrice:"$629.99",
-      productImgUrl:"https://target.scene7.com/is/image/Target/GUEST_4e327478-bf8d-4c8f-a90a-7c561597896c?qlt=85&fmt=webp&hei=325&wid=325",
+      productImgUrl:"https://target.scene7.com/is/image/Target/GUEST_4e327478-bf8d-4c8f-a90a-7c561597896c?qlt=85&fmt=webp&hei=1000&wid=1000",
       productQty:"3",
       productDescription:"(Sale) 70\" Smart 4K Crystal HDR"},
       
@@ -24,7 +24,7 @@ export class ProductlistComponent implements OnInit {
         productCategory:"Computers & Displays",
         productName: "Smart TV - V505-J09",
         productPrice:"$299.99",
-        productImgUrl:"https://target.scene7.com/is/image/Target/GUEST_e0557f9f-879e-40ac-8b9a-29b44336717e?qlt=85&fmt=webp&hei=325&wid=325",
+        productImgUrl:"https://target.scene7.com/is/image/Target/GUEST_e0557f9f-879e-40ac-8b9a-29b44336717e?qlt=85&fmt=webp&hei=1000&wid=1000",
         productQty:"2",
         productDescription:"(Sale) V-Series \" Class 4K HDR"}
       
